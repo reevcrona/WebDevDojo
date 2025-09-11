a431a6302c02c24ea527927eba0e19be7c0f6442
Jreev <jacob.reevcrona@yahoo.com>
2025-09-11 13:33:28 +0200

    remove: all users logic and schema data

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
 import Image from "next/image";
-import { createUser } from "../lib/actions";
+
 import { getCategories } from "@/lib/query";
 export default async function Home() {
   const categories = await getCategories();
 
   return (
     <div className="flex flex-col items-center">
-      <form action={createUser}>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" placeholder="name" />
-        <label htmlFor="age">Age</label>
-        <input type="text" name="age" id="age" placeholder="age" />
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" placeholder="email" />
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
+      <ul className="flex gap-6 mt-10">
         {categories.map((cat) => (
-          <li key={cat.id}>
+          <li className="max-w-[150px]" key={cat.id}>
             <Image
               src={cat.imageUrl}
               alt="Category logo"
               width={cat.imageMeta.width}
               height={cat.imageMeta.height}
+              className="rounded-2xl"
             />
           </li>
         ))}
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,35 +1,4 @@
 "use server";
 
 import { db } from "@/lib/db";
-import { users } from "@/server/db/schema/users";
 import { z } from "zod";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-const FormSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  age: z.coerce.number(),
-  email: z.string(),
-});
-
-const CreateUser = FormSchema.omit({ id: true });
-
-export async function createUser(formData: FormData) {
-  const { name, age, email } = CreateUser.parse({
-    name: formData.get("name"),
-    age: formData.get("age"),
-    email: formData.get("email"),
-  });
-
-  try {
-    await db.insert(users).values({ name, age, email });
-  } catch (error) {
-    console.error("Failed to insert user into database");
-  }
-  revalidatePath("/");
-  redirect("/");
-}
-
-export async function getUsers() {
-  return await db.select().from(users);
-}
