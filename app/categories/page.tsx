be50b42dcd3e81c418f0053eaa3ac0bce28319b3
Jreev <jacob.reevcrona@yahoo.com>
2025-09-29 13:10:07 +0200

    add: dynmic route segment using category slugs

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,5 +1,6 @@
 import { getCategories } from "@/lib/query";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Page() {
   const data = await getCategories();
@@ -9,13 +10,15 @@ export default async function Page() {
       {data.map((d) => {
         return (
           <li key={d.id}>
-            <Image
-              src={d.imageUrl}
-              alt={d.imageAlt}
-              width={d.imageMeta.width}
-              height={d.imageMeta.height}
-              className="w-60 h-60 rounded-xl"
-            />
+            <Link href={`/categories/${d.slug}`}>
+              <Image
+                src={d.imageUrl}
+                alt={d.imageAlt}
+                width={d.imageMeta.width}
+                height={d.imageMeta.height}
+                className="w-60 h-60 rounded-xl"
+              />
+            </Link>
           </li>
         );
       })}
